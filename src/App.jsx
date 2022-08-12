2268cd09c42e39004871448ce222dd16f9ede3a4
Facundo Ortiz <facundoagustinortiz@gmail.com>
2022-08-12 00:45:38 -0300

    feat/add pokes cards and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Home from './components/home/Home'
+import SearchBar from './components/searchBar/SearchBar'
 
 function App() {
 
   return (
     <div className="App">
+      <SearchBar />
       <Home />
     </div>
   )
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,14 +1,49 @@
 import React from "react";
 import styles from "./Card.module.css";
 
-const Card = ({ id, name, hp, attack, defense, speed, height, weight, image, types }) => {
+const Card = ({
+  id,
+  name,
+  hp,
+  attack,
+  defense,
+  speed,
+  height,
+  weight,
+  image,
+  types,
+}) => {
   return (
-    <div className={styles.containerCard}>
-        <div>
-            <h2>#{id}</h2>
-        </div>
+    <div
+      className={
+        types?.includes("fire")
+          ? styles.containerFire
+          : types?.includes("water")
+          ? styles.containerWater
+          : types?.includes("grass")
+          ? styles.containerGrass
+          : types?.includes("electric")
+          ? styles.containerElectric
+          : types?.includes("bug")
+          ? styles.containerBug
+          : types?.includes("normal")
+          ? styles.containerNormal
+          : types?.includes("poison")
+          ? styles.containerPoison
+          : types?.includes("ground")
+          ? styles.containerGround
+          : styles.containerNormal
+      }
+    >
       <div>
-        <img src={image} alt={name} style={{maxWidth: "150px"}}/>
+        <h2>#{id}</h2>
+      </div>
+      <div>
+        <img
+          src={image}
+          alt={name}
+        />
+        <div className={styles.name}>{name.toUpperCase()}</div>
       </div>
     </div>
   );
